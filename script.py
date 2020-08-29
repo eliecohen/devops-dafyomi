f46f177bc4cb9e0f73c12d0eef7f05a75e9bbc20
e-cohen <e.cohen@salesforce.com>
2020-08-29 23:33:48 +0300

    3

--- script.py
+++ script.py
@@ -1,6 +1,5 @@
 import os
-os.system('cmd /k "date"') 
-
+os.system('Start-ScheduledTask -TaskName ouriel') 
 f = open("c:\\dssssdfashdfkja.txt", "w")
 f.write("Now the file has more content!")
 f.close()
