5f932681392d3a6869e5bee31acd4704113acb57
e-cohen <e.cohen@salesforce.com>
2020-08-30 18:11:46 +0300

    r545

--- script.py
+++ script.py
@@ -2,6 +2,7 @@ import os
 import boto3
 import sys
 import time
+from github import Github
 
 #bookId="3"
 #dafId="91"
@@ -26,6 +27,17 @@ if os.path.isfile("c:\\output.html_files\\image.png"):
 s3 = boto3.client('s3')
 with open('c:\\flowchart.mm', 'wb') as f:
     s3.download_fileobj('daf-yomi', 'assets/upload/'+bookId+"_"+dafId+".mm", f)
+
+with open ("c:\\github_token", "r") as myfile:
+    token=myfile.readlines()[0]
+   
+book_map = {"1":"brachot","2":"shabat","3":"eruvin"}
+path = "source/"+book_map[bookId]+"/"+dafId+".mm"
+g = Github(token)
+repo = g.get_repo("eliecohen/dafyomi")
+contents = repo.get_contents(path, ref="master")
+a = repo.update_file(contents.path, "update "+path, "<elie></elie>ius",contents.sha )
+print(a)
 
 os.system('SCHTASKS.EXE /RUN /TN "ouriel"') 
 
